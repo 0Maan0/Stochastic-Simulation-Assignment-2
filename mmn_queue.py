929152aae0795e01613329b98afab02406368971
fredloth <116084074+fredloth@users.noreply.github.com>
2024-12-01 14:58:28 +0100

    pep8 rulers

--- mmn_queue.py
+++ mmn_queue.py
@@ -1,24 +1,29 @@
 """
-This module simulates and analyzes queuing systems using the SimPy library. 
-It includes the 'sims' class, which allows for the modeling of various queue configurations and 
-scheduling policies.
+This module simulates and analyzes queuing systems using the SimPy 
+library. It includes the 'sims' class, which allows for the modeling of 
+various queue configurations and scheduling policies.
 
 Key Features:
-- Models M/M/n and M/D/1 queuing systems.
-- Supports First-In-First-Out (FIFO) and Shortest Job First (SJF) scheduling.
-- Includes options for deterministic service times and long-tail service time distributions.
-- Simulates customer behavior from arrival through service completion.
+    - Models M/M/n and M/D/1 queuing systems.
+    - Supports First-In-First-Out (FIFO) and Shortest Job First (SJF) 
+    scheduling.
+    - Includes options for deterministic service times and long-tail 
+      service time distributions.
+    - Simulates customer behavior from arrival through service 
+      completion.
 
 Classes:
-- sims: Represents a queueing system with configurable parameters for arrival rate, service rate, 
-  number of servers, scheduling method, and service time distribution.
+    - sims: Represents a queueing system with configurable parameters 
+      for arrival rate, service rate, number of servers, 
+      scheduling method, and service time distribution.
 
 Dependencies:
-- numpy: Random number generation and Numerical computations.
-- simpy: Event-driven simulation of queuing system.
+    - numpy: Random number generation and Numerical computations.
+    - simpy: Event-driven simulation of queuing system.
 
 Raise:
-- Raises 'ValueError' if the system load (ρ) is unstable (i.e., ρ >= 1).
+    - Raises 'ValueError' if the system load (ρ) is unstable 
+      (i.e., ρ >= 1).
 """
 
 import numpy as np
@@ -28,33 +33,44 @@ import simpy
 
 class sims():
     """ 
-    Simulates an an M/M/n or M/M/1 queuing system with either a short-job-first or FIFO scheduling system.
+    Simulates an an M/M/n or M/M/1 queuing system with either a 
+    short-job-first or FIFO scheduling system.
 
-    This class models the arrival, service, and departure of customers in a queuing system using 
-    the SimPy library. It can handle various configurations of queuing systems, including:
-    - Multi-server queues (M/M/n)
-    - Single-server queues with deterministic service times (M/D/1)
-    - Queues with Shortest Job First (SJF) scheduling
-    - Queues with long-tail service time distributions.
+    This class models the arrival, service, and departure of customers 
+    in a queuing system using the SimPy library. It can handle various 
+    configurations of queuing systems, including:
+        - Multi-server queues (M/M/n).
+        - Single-server queues with deterministic service times (M/D/1).
+        - Queues with Shortest Job First (SJF) scheduling.
+        - Queues with long-tail service time distributions.
     """
 
 
-    def __init__(self, lam, mu, n, rho=None, sjf=False, deterministic=False, tail=False):
-        '''
-        Calculate the system load of an M/M/n queue. or M/D/1 queue if deterministic
+    def __init__(self, lam, mu, n, rho=None, sjf=False, deterministic=False, 
+                 tail=False):
+        """
+        Calculate the system load of an M/M/n queue or M/D/1 queue if
+        deterministic.
         
         Parameters:
-        lam (float): Arrival rate (λ) of customers into the system.
-        mu (float): Service rate (μ) of servers.
-        n (int): Number of servers in the system.
-        rho (float, optional): System load factor (ρ). If provided, it overrides `mu`.
-        sjf (bool, optional): Default false, determines if Shortest Job First (SJF) scheduling is enabled.
-        deterministic (bool, optional): Default false, determines if deterministic service times (M/D/1) is used.
-        tail (bool, optional): default False, determines if a long-tail (hyperexponential) distribution for service times is used.
+            - lam (float): Arrival rate (λ) of customers into the
+              system.
+            - mu (float): Service rate (μ) of servers.
+            - n (int): Number of servers in the system.
+            - rho (float, optional): System load factor (ρ). If
+              provided, it overrides `mu`.
+            - sjf (bool, optional): Default false, determines if
+              Shortest Job First (SJF) scheduling is enabled.
+            - deterministic (bool, optional): Default false, determines
+              if deterministic service times (M/D/1) is used.
+            - tail (bool, optional): default False, determines if a
+              long-tail (hyperexponential) distribution for service 
+              times is used.
 
         Raises:
-            ValueError: If the system load factor (ρ) is greater than or equal to 1, indicating an unstable system.
-        '''
+            ValueError: If the system load factor (ρ) is greater than or
+                        equal to 1, indicating an unstable system.
+        """
 
         self.lam = lam
         self.n = n
@@ -78,25 +94,29 @@ class sims():
 
     def service(self, service_time, priority):
         '''
-        Models the service process for a single customer, including their waiting time and service duration.
+        Models the service process for a single customer, including
+        their waiting time and service duration.
         '''
 
         arrive = self.env.now
 
         with self.server.request(priority=priority) as req:
-            yield req #Wait until the request is granted.
+            #Wait until the request is granted.
+            yield req 
             wait = self.env.now - arrive
 
             self.waiting_times.append(wait)
             self.service_time_list.append(service_time)
 
-            yield self.env.timeout(service_time) #Wait for the service to finish.
+            #Wait for the service to finish.
+            yield self.env.timeout(service_time)
 
     
     def source(self, max_customers):
-        '''
-        Generates customer arrivals according to the specified arrival rate (λ).
-        '''
+        """
+        Generates customer arrivals according to the specified arrival
+        rate (λ).
+        """
 
         for _ in range(max_customers):
             #Determine the time between arrivals.
@@ -128,10 +148,11 @@ class sims():
 
 
     def simulate_queue(self, max_customers = 10000, seed=None):
-        '''
-        Simulates the queueing system and returns a list of waiting times for all customers.
+        """
+        Simulates the queueing system and returns a list of waiting 
+        times for all customers.
         Return: list of waiting times.
-        '''
+        """
 
         #Lists used for tracking the simulation.
         self.waiting_times = [] #Time waited, result of sim.
--- plots.py
+++ plots.py
@@ -1,13 +1,16 @@
 """
 This module visualizes the performance of simulated queuing systems 
-based on the 'mmn_queue.py' module. Including code to plot mean waiting times across multiple 
-simulations for different configurations of queueing systems and a plot of SFJ vs FIFO scheduling systems for the same parameters.
+based on the 'mmn_queue.py' module. Including code to plot mean waiting 
+times across multiple simulations for different configurations of 
+queueing systems and a plot of SFJ vs FIFO scheduling systems for the 
+same parameters.
 
 Key Features:
-- Generates plots to compare the effect of different server counts (n) and system load factors (ρ) 
-  on mean waiting times.
-- Supports various queuing methods, including FIFO (First-In-First-Out), SJF (Shortest Job First), 
-  deterministic service times (M/D/1), and long-tail service time distributions.
+    - Generates plots to compare the effect of different server counts 
+      (n) and system load factors (ρ) on mean waiting times.
+    - Supports various queuing methods, including FIFO 
+      (First-In-First-Out), SJF (Shortest Job First), deterministic 
+      service times (M/D/1), and long-tail service time distributions.
 
 Usage Example:
     - Run the module to generate plots for FIFO and SJF queueing methods.
@@ -20,7 +23,8 @@ Dependencies:
 
 The generated plots display:
     - Mean waiting times with standard deviation bands.
-    - Subplots for different server configurations (n), with legends for various 'ρ' values.
+    - Subplots for different server configurations (n), with legends for 
+      various 'ρ' values.
 """
 
 
@@ -32,27 +36,35 @@ from mmn_queue import sims
 #plt.rc('font', family='serif')
 
 
-def plot_mean_waiting_times(lam, mu, n_list, rho_values, num_simulations, max_customers, starting_amount, method = 'fifo'):
+def plot_mean_waiting_times(lam, mu, n_list, rho_values, num_simulations, 
+                            max_customers, starting_amount, method = 'fifo'):
     """
-    Plot the mean waiting times across multiple simulations for different system configurations.
+    Plot the mean waiting times across multiple simulations for 
+    different system configurations.
 
     Parameters:
-        lam (float): Arrival rate (λ) of customers into the system.
-        mu (float): Service rate (μ) of servers.
-        n_list (list[int]): List of server counts (`n`) to simulate.
-        rho_values (list[float]): List of load factors (`ρ`) to simulate.
-        num_simulations (int): Total number of simulations to run for each configuration.
-        max_customers (int): Maximum number of customers per simulation.
-        starting_amount (int): Number of initial simulations to exclude from statistical analysis.
-        method (str, optional): Queueing method. Options are:
+        - lam (float): Arrival rate (λ) of customers into the system.
+        - mu (float): Service rate (μ) of servers.
+        - n_list (list[int]): List of server counts (`n`) to simulate.
+        - rho_values (list[float]): List of load factors (`ρ`) to 
+          simulate.
+        - num_simulations (int): Total number of simulations to run for 
+          each configuration.
+        - max_customers (int): Maximum number of customers per 
+          simulation.
+        - starting_amount (int): Number of initial simulations to 
+          exclude from statistical analysis.
+        - method (str, optional): Queueing method. Options are:
             - 'fifo': First-In-First-Out (default).
             - 'sjf': Shortest Job First.
             - 'deterministic': Deterministic service times (M/D/1).
-            - 'tail': Long-tail (hyperexponential) service time distribution.
+            - 'tail': Long-tail (hyperexponential) service time 
+              distribution.
 
     Outputs:
-        A PDF plot, saved to `Figures/` directory, showing mean waiting times and their standard 
-        deviations as shaded regions for different values of 'ρ' and 'n'.
+        A PDF plot, saved to `Figures/` directory, showing mean waiting 
+        times and their standard deviations as shaded regions for 
+        different values of 'ρ' and 'n'.
 
     Raises:
         ValueError: If an invalid `method` is provided.
@@ -62,7 +74,8 @@ def plot_mean_waiting_times(lam, mu, n_list, rho_values, num_simulations, max_cu
     COLORS = ['tab:blue', 'tab:orange', 'tab:green']  
     hfont = {'fontname':'Serif'}
 
-    fig, axes = plt.subplots(1, len(rho_values), figsize=(len(rho_values)*5, 5), sharey=True)
+    fig, axes = plt.subplots(1, len(rho_values), 
+                             figsize=(len(rho_values)*5, 5), sharey=True)
 
     for i, n in enumerate(n_list):
         ax = axes[i]
@@ -90,20 +103,22 @@ def plot_mean_waiting_times(lam, mu, n_list, rho_values, num_simulations, max_cu
                 simulation = sim.simulate_queue(max_customers=max_customers)
                 mean_waiting_times.append(np.mean(simulation))
 
-                #Start calculating mean and std after starting_amount simulations.
+                #Calculate mean and std after starting_amount simulations.
                 if len(mean_waiting_times) >= starting_amount:
-                    #Calculate mean and std of mean waiting times.
                     current_mean = np.mean(mean_waiting_times)
                     current_std = np.std(mean_waiting_times, ddof=1)
                     mean_values.append(current_mean)
                     std_values.append(current_std)
 
             #X-array for number of simulations.
-            x_values = np.linspace(starting_amount, num_simulations, num=len(mean_values))
+            x_values = np.linspace(starting_amount, num_simulations, 
+                                   num=len(mean_values))
             mean_values = np.array(mean_values)
             std_values = np.array(std_values)
 
-            ax.fill_between(x_values, mean_values - std_values, mean_values + std_values, color=COLORS[j], alpha=0.2, label=rf'$\rho={rho}$')
+            ax.fill_between(x_values, mean_values - std_values, mean_values 
+                            + std_values, color=COLORS[j], alpha=0.2, 
+                            label=rf'$\rho={rho}$')
             ax.plot(x_values, mean_values, color=COLORS[j])
 
             ax.axhline(np.mean(mean_values), color=COLORS[j], linestyle='--')
@@ -115,13 +130,15 @@ def plot_mean_waiting_times(lam, mu, n_list, rho_values, num_simulations, max_cu
             if ax == axes[1]:
                 ax.set_xlabel('Number of Simulations', fontsize=FONTSIZE)
         
-            ax.text(0.95, 0.95, f'n ={n}', transform=ax.transAxes, fontsize=FONTSIZE-3,
-            verticalalignment='top', horizontalalignment='right')
+            ax.text(0.95, 0.95, f'n ={n}', transform=ax.transAxes, 
+            fontsize=FONTSIZE-3, verticalalignment='top', 
+            horizontalalignment='right')
             ax.tick_params(axis='x', labelsize=FONTSIZE-4)
             ax.tick_params(axis='y', labelsize=FONTSIZE-4)
 
     plt.tight_layout()
-    plt.savefig(f'Figures\mean_waiting_times_{method}.pdf', bbox_inches='tight', format='pdf')
+    plt.savefig(f'Figures\mean_waiting_times_{method}.pdf', 
+                bbox_inches='tight', format='pdf')
     plt.show()
 
 
@@ -130,16 +147,17 @@ def plot_sfj_vs_fifo(lam, mu, n, rho=0.9):
     Compare waiting times in SJF and FIFO scheduling systems.
 
     Parameters:
-        lam (float): Arrival rate (λ) of customers into the system.
-        mu (float): Service rate (μ) of servers.
-        n (int): Number of servers in the system.
-        rho (float, optional): System load factor (ρ). Default is 0.9.
+        - lam (float): Arrival rate (λ) of customers into the system.
+        - mu (float): Service rate (μ) of servers.
+        - n (int): Number of servers in the system.
+        - rho (float, optional): System load factor (ρ). Default is 0.9.
 
     Outputs:
-        A PDF plot saved to the `Figures/` directory with the filename `sjf_vs_fifo_n{n}_rho{rho}.pdf`.
+        - A PDF plot saved to the 'Figures/' directory with the filename 
+          'sjf_vs_fifo_n{n}_rho{rho}.pdf'.
         The plot contains two subplots:
-        - Left: SJF waiting times vs. service times.
-        - Right: FIFO waiting times vs. service times.
+            - Left: SJF waiting times vs. service times.
+            - Right: FIFO waiting times vs. service times.
     """
 
     #Perform SJF simulation.
@@ -150,10 +168,13 @@ def plot_sfj_vs_fifo(lam, mu, n, rho=0.9):
     sim_fifo = sims(lam, mu, n, rho, sjf=False)
     sim_fifo.simulate_queue()
 
-    fig, ax = plt.subplots(1, 2, sharey=True, sharex=True, gridspec_kw={'wspace': 0.05})
+    fig, ax = plt.subplots(1, 2, sharey=True, sharex=True,
+                           gridspec_kw={'wspace': 0.05})
 
-    ax[0].scatter(sim_sjf.service_time_list, sim_sjf.waiting_times, marker='.', label='SJF', s=50, lw=0)
-    ax[1].scatter(sim_fifo.service_time_list, sim_fifo.waiting_times, marker='.', label='FIFO', s=50, lw=0)
+    ax[0].scatter(sim_sjf.service_time_list, sim_sjf.waiting_times, marker='.',
+                  label='SJF', s=50, lw=0)
+    ax[1].scatter(sim_fifo.service_time_list, sim_fifo.waiting_times, 
+                  marker='.', label='FIFO', s=50, lw=0)
     ax[0].set_ylabel(r'Waiting Time ($t_w$)')
     ax[0].set_xlabel(r'Service Time ($t_s$)')
     ax[1].set_xlabel(r'Service Time ($t_s$)')
@@ -169,7 +190,7 @@ def plot_sfj_vs_fifo(lam, mu, n, rho=0.9):
     return
 
 
-#Set appropriate parameters: lambda, mu, rho, number of simulations, max customers and starting value.
+#Set appropriate parameters.
 LAMBDA = 1
 MU = 1.2
 rho_list = [0.8, 0.9, 0.99]
@@ -183,10 +204,11 @@ mu = 1.2
 n = 1
 
 #Plot for FIFO.
-#plot_mean_waiting_times(LAMBDA, MU, n_list, rho_list, NUM_SIM, MAX_CUST, START)
+plot_mean_waiting_times(LAMBDA, MU, n_list, rho_list, NUM_SIM, MAX_CUST, START)
 
 #Plot for SJF.
-#plot_mean_waiting_times(LAMBDA, MU, n_list, rho_list, NUM_SIM, MAX_CUST, START, method='sjf')
+plot_mean_waiting_times(LAMBDA, MU, n_list, rho_list, NUM_SIM, MAX_CUST, 
+                        START, method='sjf')
 
 #Plot SFJ vs FIFO.
 plot_sfj_vs_fifo(lam, mu, n, rho=0.9)
